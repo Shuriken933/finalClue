11ecdc7a2ded929a77cd77043aecce3d41a72051
Eros <agostini.eros@tiscali.it>
2025-02-09 15:45:55 +0100

    minor fixes:  - 💀 removed ClueFactory empty constructor  - 🤦 small grammatical error "attemps" -> "attempts"

--- src/Game.java
+++ src/Game.java
@@ -8,65 +8,65 @@ public class Game {
 	private Player[] allPlayers;
 	private CrimeSolution crimeSolution;
 	private boolean isCrimeOpen;
-	
+
 	public Game() {
 		this.isCrimeOpen = true;
 		ClueFactory clueFactory = new ClueFactory();
 		this.allSuspects = clueFactory.createSuspects();
 		this.allWeapons = clueFactory.createWeapons();
 		this.allRooms = clueFactory.createRooms();
-		
+
 		this.crimeSolution = new CrimeSolution(allSuspects, allWeapons, allRooms);
 	}
-	
+
 	public void play() {
 		System.out.println("Benvenuto nel gioco!");
 		showAllClues();
-		
+
 		this.crimeSolution.showTheSolution();
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
-		int countAttemps = 0;
-		
+
+		int countAttempts = 0;
+
 		do {
-			countAttemps++;
-			
+			countAttempts++;
+
 			System.out.print("\n\nInserisci il nome del sospettato: ");
 			String suspectName = scanner.nextLine();
 			System.out.print("\n\nInserisci il nome dell'arma: ");
 			String weapontName = scanner.nextLine();
 			System.out.print("\n\nInserisci il nome della stanza: ");
 			String roomName = scanner.nextLine();
-			
+
 			makeSuggestion(suspectName, weapontName, roomName);
-			
-			if(!(countAttemps<5)) {isCrimeOpen = false;}
-			
-		} while(isCrimeOpen && countAttemps<5);
-		
-		
+
+			if(!(countAttempts<5)) {isCrimeOpen = false;}
+
+		} while(isCrimeOpen && countAttempts<5);
+
+
 		scanner.close();
 
 	}
-	
+
 	private void showAllClues() {
 		System.out.print("\nTutti i sospettati: ");
 		for (Suspect suspect : this.allSuspects) {
 			System.out.print(suspect.getName() + " - ");
 		}
-		
+
 		System.out.print("\n\nTutte le armi: ");
 		for (Weapon weapon : this.allWeapons) {
 			System.out.print(weapon.getName() + " - ");
 		}
-		
+
 		System.out.print("\n\nTutte le stanze: ");
 		for (Room room : this.allRooms) {
 			System.out.print(room.getName() + " - ");
 		}
 	}
-	
+
 	private void makeSuggestion(String suspect, String weapon, String room) {
 		if(this.crimeSolution.isTheAssassin(suspect) && this.crimeSolution.isTheMurderWeapon(weapon) && this.crimeSolution.isTheCrimeScene(room)) {
 			System.out.println("Complimenti! Hai risolto il caso!");
